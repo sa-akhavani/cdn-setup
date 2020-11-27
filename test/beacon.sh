4a01902b6bcefc99a0e3a686a283d4196b0e7905
Anthony Gavazzi <anthonygavaz@yahoo.com>
2020-11-27 10:44:02 -0500

    dns and http server scripts + beacon script

---
+++ test/beacon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NS=cs5700cdnproject.ccs.neu.edu
+NAME=cs5700cdn.example.com
+
+#for port in {40001..40030}; do
+for port in $1; do
+    echo "port $port"
+        IP=`dig +short +time=2 +tries=1 -p $port @$NS $NAME | head -1`
+        if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+           wget -O /dev/null http://$IP:$port/wiki/Main_Page
+        fi
+        sleep 1
+done
