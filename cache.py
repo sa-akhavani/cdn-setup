1ab35590a5524dcb46a3f074ef3be1ee5c99bac6
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2020-12-02 11:23:27 -0500

    Add memory limit management to cache

--- cache.py
+++ cache.py
@@ -1,23 +1,28 @@
 from collections import OrderedDict
 import sys
 
+# todo compress things in the cache
+
 class LRUCache:
     # initialising capacity
     def __init__(self, memory_limit: int):
         self.cache = OrderedDict()
         self.memory_limit_bytes = memory_limit
+        self.used_memory = 0
 
     def get(self, key: str):
         if key not in self.cache:
             return -1
         else:
-            self.cache.move_to_end(key)
+            # self.cache.move_to_end(key)
             return self.cache[key]
 
     def put(self, key: str, value: str):
-        self.cache[key] = value
-        self.cache.move_to_end(key)
-        
-        # process_current_memory_usage = sys.getsizeof(self.cache)
-        # if process_current_memory_usage > self.memory_limit_bytes:
-        #     self.cache.popitem(last = False)
+        if self.used_memory + len(value) < self.memory_limit_bytes:
+            self.cache[key] = value
+            self.used_memory += len(value)
+            # self.cache.move_to_end(key)
+            return 0
+        else:
+            print('reached memory limit')
+            return -1
--- httpserver.py
+++ httpserver.py
@@ -1,20 +1,23 @@
 from flask import Flask
 import requests
-from sample import html_file
 import cache
+import sys
 
 origin_server_ip = '18.207.254.152'
 origin_server_port = 8080
+cache_memory_limit = 10485760
 
 app = Flask(__name__)
-cache = cache.LRUCache(10000000)
+cache = cache.LRUCache(cache_memory_limit)
 
 @app.route('/wiki/<name>')
 def test(name):
     content = cache.get(name)
     if  content != -1:
+        print('cache hit!')
         return content
     else:
+        print('cache miss')
         page_content = requests.get('http://{}:{}/wiki/{}'.format(origin_server_ip, origin_server_port, name)).content
         cache.put(name, page_content)
         return page_content
