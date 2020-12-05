590d89046f46bbcb2719acd13e262c245aa22cd7
Anthony Gavazzi <anthonygavaz@yahoo.com>
2020-12-05 12:40:58 -0500

    makefile + removed helpers in cache

--- cache.py
+++ cache.py
@@ -5,35 +5,28 @@ class Cache:
     """
     Implements our cache, which maps wikipedia article names as strings to compressed HTML bodies for those articles
 
-    The cache also manages its own size and ensures that it never stores more data than it is allowed to.
+    The cache also manages its own size and ensures that it never stores more data than is specified in its constructor
     """
 
     # initialising capacity
     def __init__(self, memory_limit):
         self.cache = dict()
-        self.memory_limit_bytes = memory_limit
+        self.memory_limit_bytes = memory_limit  # in bytes
         self.used_memory = 0
 
-    # Compress string before putting them in the cache
-    def __compress(self, value):
-        return zlib.compress(value)
-
-    # Decompress string before returning to caller
-    def __decompress(self, value):
-        original = zlib.decompress(value)
-        return original.decode()
-
-    # Get value of the key from cache
     def get(self, key):
+        """Returns the decompressed value of the key in the cache, or -1 if it is not in the cache"""
         if key not in self.cache:
             return -1
         else:
-            return self.__decompress(self.cache[key])
+            return zlib.decompress(self.cache[key])
 
-    # Put value for the key in cache
     def put(self, key, value):
-        compressed = self.__compress(value)
-        if self.used_memory + len(compressed) < self.memory_limit_bytes:
+        """Compresses the given value and puts it in the cache under the given key if there is room for it,
+        returning -1 if there is not room"""
+        compressed = zlib.compress(value)
+
+        if self.used_memory + len(compressed) <= self.memory_limit_bytes:
             self.cache[key] = compressed
             self.used_memory += len(compressed)
             return 0
--- httpserver.py
+++ httpserver.py
@@ -3,6 +3,7 @@ import argparse
 from flask import Flask
 import requests
 import cache
+import csv
 
 # ec2-18-207-254-152.compute-1.amazonaws.com will be provided from the command line
 # should resolve to '18.207.254.152'
@@ -12,6 +13,17 @@ cache_memory_limit = 10485760
 
 app = Flask(__name__)
 cache = cache.Cache(cache_memory_limit)
+
+
+def fillcache():
+    """Requests the most popular wikipedia pages from the origin server until the cache contains 10MB of data"""
+    with open('/course/cs5700f20/popular_pages_20201130.csv', 'r') as fp:
+        reader = csv.reader(fp)
+        for row in reader:
+            print(row[0].split('/'))
+
+            # TODO request page and add to cache (do this for the entire file)
+            # or maybe do knapsack solution with weight as article size and value as number of hits
 
 
 @app.route('/wiki/<name>')
