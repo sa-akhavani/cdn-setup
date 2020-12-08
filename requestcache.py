e761b48ca4bf7b62efd878b65314c798ced5764b
Anthony Gavazzi <anthonygavaz@yahoo.com>
2020-12-08 18:25:31 -0500

    code to send requests to the origin server and build cache

---
+++ requestcache.py
@@ -0,0 +1,33 @@
+import argparse
+import csv
+import urllib.request
+import zlib
+
+import fileifc
+
+
+def run(args: argparse.Namespace):
+    with open(args.i, 'r') as fp:
+        wrtr = fileifc.Writer(args.o)
+
+        rdr = csv.reader(fp)
+
+        for row in rdr:
+            for article in row:
+                response = urllib.request.urlopen('http://{}:8080/wiki/{}'.format(args.n, article))
+                content = response.read()
+                wrtr.write(article, zlib.compress(content))
+
+        fp.close()
+        wrtr.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Requests all the articles names in the given csv file from the origin'
+                                                 'server and writes the compressed article bodies to a JSON')
+    parser.add_argument('-i', required=True, help='input file name')
+    parser.add_argument('-o', required=True, help='output file name')
+    parser.add_argument('-n', required=False, default='ec2-18-207-254-152.compute-1.amazonaws.com', help='origin server address')
+    args = parser.parse_args()
+
+    run(args)
---
+++ test/testcachebuilder.py
@@ -0,0 +1,26 @@
+import argparse
+
+import fileifc
+import requestcache
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tests requestcache')
+    parser.add_argument('-i', required=True, help='input file name')
+    parser.add_argument('-o', required=True, help='output file name')
+    parser.add_argument('-n', required=False, default='ec2-18-207-254-152.compute-1.amazonaws.com',
+                        help='origin server address')
+    parser.add_argument('-skip', required=False, type=bool, default=True, help='skip requesting files')
+    args = parser.parse_args()
+
+    if not args.skip:
+        requestcache.run(args)
+
+    rdr = fileifc.Reader(args.o)
+    objlist = rdr.read()
+
+    sz = 0
+    for obj in objlist:
+        sz += len(obj['body'])
+
+    print(sz < 10485760)
