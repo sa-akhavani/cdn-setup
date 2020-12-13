6e3ba29efea46f7a77ad31fba3665e49333faa67
Anthony Gavazzi <gavazzi.a@husky.neu.edu>
2020-12-13 16:30:06 -0500

    tested scripts on two servers. successfully deploys, runs, and kills

--- commonCDN.py
+++ commonCDN.py
@@ -11,10 +11,10 @@ PIDFILE = 'pid.txt'
 
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser(description='Deploys all necessary files to the replica servers')
+    parser = argparse.ArgumentParser()
     parser.add_argument('-p', required=True, type=int, help='port')
     parser.add_argument('-o', required=True, help='origin')
     parser.add_argument('-n', required=True, help='name')
     parser.add_argument('-u', required=True, help='username')
     parser.add_argument('-i', required=True, help='keyfile')
-    return parser.parse_args(argv)
+    return parser.parse_args(argv[1:])
